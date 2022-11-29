a767baaa8728063d372aff0b69e3f5e8335a79bc
xCorz <corey_239@hotmail.com>
2022-11-29 14:56:57 +1100

    final

--- Speedtest/Speedtest.py
+++ Speedtest/Speedtest.py
@@ -2,6 +2,6 @@ import speedtest
 
 wifi = speedtest.Speedtest()
 wifi.get_best_server()
-print("Wifi download speed = {round(wifi.download() / 1000 / 1000, 1)} MB/s")
-print("Wifi upload speed = {round(wifi.upload() / 1000 / 1000, 1)} MB/s")
-print("Ping = {wifi.results.ping} ms")
+print(f"Wifi download speed = {round((wifi.download() / 1000 / 1000) / 8, 1)} MB/s")
+print(f"Wifi upload speed = {round((wifi.upload() / 1000 / 1000) / 8, 1)} MB/s")
+print(f"Ping = {wifi.results.ping} ms")
